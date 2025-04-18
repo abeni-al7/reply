6e370ce43b0c8c2c92b1f489c86ef761b094bf22
abeni-al7 <abenezeralebachew3@gmail.com>
2025-04-18 12:22:11 +0300

    Add previews for different screen sizes

--- app/src/main/java/com/example/reply/MainActivity.kt
+++ app/src/main/java/com/example/reply/MainActivity.kt
@@ -65,11 +65,33 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(
-    showBackground = true,
-)
+@Preview(showBackground = true)
 @Composable
 fun ReplyAppCompactPreview() {
+    ReplyTheme {
+        Surface {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 700)
+@Composable
+fun ReplyAppMediumPreview() {
+    ReplyTheme {
+        Surface {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Medium
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 1000)
+@Composable
+fun ReplyAppExpandedPreview() {
     ReplyTheme {
         Surface {
             ReplyApp(
